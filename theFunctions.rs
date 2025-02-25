1497ed5473a1c03c625c8d9b8dff03769ee55382
Trevor_143 <trevorarapu2@gmail.com>
2025-02-25 14:32:17 +0300

    done with functions

--- theFunctions.rs
+++ theFunctions.rs
@@ -5,13 +5,20 @@
 fn main() {
     // hello();
     // tell_height(210);
-    human_id("Trevor Arapu", 30, 220.2, "trevorarapu2@gmail.com")
+    // human_id("Trevor Arapu", 30, 220.2, "trevorarapu2@gmail.com")
+
+    let _x: i32 = {
+        let price: i32 = 5;
+        let qty: i32 = 10;
+        price*qty
+    };
+    println!("Result is: {}", _x);
 }
 
 // fn tell_height(height: u32) {
 //     println!("My height is {}", height);
 // }
 
-fn human_id(name: &str, age: u32, height: f32, email: &str) {
-    println!("My name is {}, email {} plus am {} years old and about {}cm tall.", name, email, age, height)
-}
+// fn human_id(name: &str, age: u32, height: f32, email: &str) {
+//     println!("My name is {}, email {} plus am {} years old and about {}cm tall.", name, email, age, height)
+// }

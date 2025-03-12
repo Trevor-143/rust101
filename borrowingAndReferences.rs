08374719211cf64f6559b5c2057e04587f870f7a
Trevor_143 <trevorarapu2@gmail.com>
2025-03-12 18:56:36 +0300

    redo of the function

--- borrowingAndReferences.rs
+++ borrowingAndReferences.rs
@@ -8,29 +8,65 @@
 
 // }
 
+// fn main() {
+//     let mut account: BankAccount = BankAccount{
+//         owner: "Alice".to_string(),
+//         balance: 1050.43,
+//     };
+
+//     account.check_balance();
+//     account.withdraw(500.2);
+//     account.check_balance();
+// }
+
+// struct BankAccount {
+//     owner: String,
+//     balance: f64,
+// }
+
+// impl BankAccount {
+//     fn withdraw(&mut self, amount: f64) {
+//         println!("Withdraewing {} from account owned by {}", amount, self.owner );
+//         self.balance -= amount;
+//     }
+
+//     fn check_balance(&self) {
+//         println!("Account owned by {} has a balance of {}", self.owner, self.balance)
+//     }
+// }
+
 fn main() {
     let mut account: BankAccount = BankAccount{
-        owner: "Alice".to_string(),
-        balance: 1050.43,
+        owner: "Trevor".to_string(),
+        balance: 100000.00
     };
 
-    account.check_balance();
-    account.withdraw(500.2);
-    account.check_balance();
+    account.check_cash();
+    account.withdraw_cash(50000.00);
+    account.check_cash();
+    account.add_cash(200000.00);
+    account.check_cash();
 }
 
 struct BankAccount {
     owner: String,
-    balance: f64,
+    balance: f64
 }
 
 impl BankAccount {
-    fn withdraw(&mut self, amount: f64) {
-        println!("Withdraewing {} from account owned by {}", amount, self.owner );
+
+    fn withdraw_cash(&mut self, amount: f64) {
+        println!("Your withdrawing {:.2} from {}'s Account", amount, self.owner);
         self.balance -= amount;
     }
 
-    fn check_balance(&self) {
-        println!("Account owned by {} has a balance of {}", self.owner, self.balance)
+    fn add_cash(&mut self, amount: f64) {
+        println!("Your adding {:.2} to {}'s account", amount, self.owner);
+        self.balance += amount;
     }
+
+    fn check_cash(&mut self) {
+        println!("Currently {} has {:.2} in their account", self.owner, self.balance);
+    }
+
 }
